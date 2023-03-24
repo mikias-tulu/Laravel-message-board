ff671f7d1be181d83c2f5e9965e1275a42a4ddc9
Miki T <miki.tulu@gmail.com>
2023-03-24 19:01:40 +0300

    routes created

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route:: view(uri: '/about' , view: 'about')-> name ('about');
+Route::view(uri: '/contact' , view: 'contact')-> name ('contact');
